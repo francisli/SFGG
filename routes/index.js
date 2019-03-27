51bec0e467278f41d122dc508f14b20b340a4562
Francis Li <mail@francisli.com>
2019-03-26 17:18:51 -0700

    Stylized homepage and navigation buttons!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Can u c this?' });
+  models.Garden.all().then(function(gardens) {
+    res.render('index', {
+      title: 'SF Green Grounds',
+      gardens: gardens
+    });
+  });
+
 });
 
 module.exports = router;
